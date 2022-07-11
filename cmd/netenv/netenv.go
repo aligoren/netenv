c61814f5d94f9331f3d06edb6ebefe8c9f72943c
Ali GOREN <goren.ali@yandex.com>
2022-07-11 16:16:34 +0300

    added port config to yaml file and parsed it

--- cmd/netenv/netenv.go
+++ cmd/netenv/netenv.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"github.com/aligoren/netenv/config"
 	"github.com/aligoren/netenv/internal/server"
 	"github.com/aligoren/netenv/pkg/logger"
 )
 
 func main() {
 
-	server := server.New(":8080")
+	cfg, err := config.GetConfig()
+	if err != nil {
+		logger.Get().Error(err)
+	}
 
-	err := server.Start()
+	server := server.New(cfg.Global.Addr)
+
+	err = server.Start()
 	if err != nil {
 		logger.Get().Error(err)
 	}
--- config/config.go
+++ config/config.go
@@ -7,6 +7,7 @@ import (
 )
 
 type global struct {
+	Addr string
 	Auth auth
 }
 
